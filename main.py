16de8fd152f42f10fe1b432d8b97db8333cb86e1
Valkers99 <byinjuri@gmail.com>
2023-11-19 22:54:52 +0100

    More bot features for funny responses, also sends as image instead of as URL

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ import discord
 from decouple import config
 import requests
 from bs4 import BeautifulSoup
+import io
+from PIL import Image
+import aiohttp
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -26,7 +29,14 @@ async def on_message(message):
             post = randint(0,24)
             response = requests.get(f"https://gelbooru.com/index.php?page=dapi&s=post&q=index&tags=foot_focus&limit=25&pid={page}")
             unsouped_response = BeautifulSoup(response.content, features='xml')
-            await message.channel.send(str(unsouped_response.find_all('file_url')[post])[10:-11])
+            image_url = str(unsouped_response.find_all('file_url')[post])[10:-11]
+            async with aiohttp.ClientSession() as session:
+                async with session.get(image_url) as resp:
+                    if resp.status != 200:
+                        return await message.channel.send('Could not download file...')
+                    data = io.BytesIO(await resp.read())
+                    await message.channel.send(file=discord.File(data, 'cool_image.png'))
+
 
     if message.author.name == "totallynotbob" and not(message.content.startswith('t!')):
         tenpercenter = randint(0,9)
@@ -35,7 +45,29 @@ async def on_message(message):
             post = randint(0,24)
             response = requests.get(f"https://gelbooru.com/index.php?page=dapi&s=post&q=index&tags=suit 1girl solo helltaker&limit=25&pid={page}")
             unsouped_response = BeautifulSoup(response.content, features='xml')
-            await message.channel.send(str(unsouped_response.find_all('file_url')[post])[10:-11])
+            image_url = str(unsouped_response.find_all('file_url')[post])[10:-11]
+            async with aiohttp.ClientSession() as session:
+                async with session.get(image_url) as resp:
+                    if resp.status != 200:
+                        return await message.channel.send('Could not download file...')
+                    data = io.BytesIO(await resp.read())
+                    await message.channel.send(file=discord.File(data, 'cool_image.png'))
+
+
+    if message.author.name == "valkers" and not(message.content.startswith('t!')):
+        tenpercenter = randint(0,9)
+        if tenpercenter == 0:
+            page = randint(0,5)
+            post = randint(0,24)
+            response = requests.get(f"https://gelbooru.com/index.php?page=dapi&s=post&q=index&tags=futa solo&limit=25&pid={page}")
+            unsouped_response = BeautifulSoup(response.content, features='xml')
+            image_url = str(unsouped_response.find_all('file_url')[post])[10:-11]
+            async with aiohttp.ClientSession() as session:
+                async with session.get(image_url) as resp:
+                    if resp.status != 200:
+                        return await message.channel.send('Could not download file...')
+                    data = io.BytesIO(await resp.read())
+                    await message.channel.send(file=discord.File(data, 'SPOILER_cool_image.png'))
 
     if message.content.startswith('t!'):
         command = message.content[3:]
